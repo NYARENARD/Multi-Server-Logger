f27f1da5a1ccbf91b863d36133f552ebc6aa0597
NYARENARD <82282414+NYARENARD@users.noreply.github.com>
2022-11-17 20:34:47 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,112 +1,95 @@
 import selfcord
 import io
 import aiohttp
-import requests
 from secondary import config
+
 
 log_guild = config["log_guild"]
 aux_channel = config["aux_channel"]
 
+
 class Multi_Server_Logger(selfcord.Client):
     async def on_ready(self):
-        print(f'Logged in as {self.user} (ID: {self.user.id})')
-        print('------')
+        print(f'Logged in: {self.user} (Selfbot)')
     
+
     async def on_error(self, event):
         print(f"Error: {event}")
 
-    async def create_on_events(self, channel):
-        gu = channel.guild
-        ch = channel
+
+    async def create_get_channel(self, ch):
+        gu = ch.guild
         serv = self.get_guild(log_guild)
-        aux = self.get_channel(aux_channel)
         if gu == serv:
             return None
 
-        messages = [msg async for msg in aux.history(limit=200)]
-        exists = 0
-        for msg in messages:
-            if f"GUILD {gu.id}" in msg.content:
-                exists = 1
-                pointer = msg
+        category = None
+        for c in serv.channels:
+            if c.name == serv.name:
+                category = c
                 break
-        if exists:
-            category = self.get_channel(int(pointer.content.split()[2]))
-        else:
+        if not category:
             category = await serv.create_category(gu.name)
-            await aux.send(f"GUILD {gu.id} {category.id} ({gu.name})")
-        
-        exists = 0
-        for msg in messages:
-            if f"CHANNEL {ch.id}" in msg.content:
-                exists = 1
-                pointer = msg
+
+        channel = None
+        for c in serv.channels:
+            if c.name == ch.name:
+                channel = c
                 break
-        if exists:
-            new_ch = self.get_channel(int(pointer.content.split()[2]))
-        else:
-            new_ch = await serv.create_text_channel(ch.name, category=category)
-            await aux.send(f"CHANNEL {ch.id} {new_ch.id} ({gu.name} -- {ch.name})")
-        return new_ch
+        if not channel:
+            channel = await serv.create_text_channel(ch.name, category=category)
+        return channel
+        
 
     async def on_typing(self, channel, user, when):
-        if user.id == self.user.id:
+        if self.user.id == user.id or log_guild == channel.guild.id:
             return
-        try:
-            ch = await self.create_on_events(channel)
-        except:
-            return
+        ch = await self.create_get_channel(channel)
         await ch.trigger_typing()
 
+
+    def parse_content(self, string):
+        return string.replace(("@everyone", "@ evryone")).replace("@here", "@ her")
+
+
     async def on_message(self, message):
-        if message.author.id == self.user.id:
-            return
-        try:
-            ch = await self.create_on_events(message.channel)
-        except:
+        if message.author.id == self.user.id or message.guild.id == log_guild:
             return
 
-        try:
-            fp = requests.get(message.author.display_avatar.url).content
-            emoji = await self.get_guild(log_guild).create_custom_emoji(name="avatar", image=fp)
-            subload = f"<:{emoji.name}:{emoji.id}>"
-        except:
-            subload = ""
+        ch = await self.create_get_channel(message.channel)
+
         att = []
         for a in message.attachments:
             async with aiohttp.ClientSession() as session:
                 async with session.get(a.url) as resp:
                     att.append(selfcord.File(io.BytesIO(await resp.read()), a.filename))
        
-        payload = "`MSG " + f"{message.author.name}#{message.author.discriminator}`".rjust(21) + f"{subload}"
+        message.content = self.parse_content(message.content)
+        payload = "`MSG " + f"{message.author.name}#{message.author.discriminator}`".rjust(21)
+        
         if message.reference == None:
-            await ch.send(payload + f": {message.content}".replace("@everyone", "@ evryone"), files=att)
+            await ch.send(payload + f": {message.content}", files=att)
         else:
-            messages = [msg async for msg in ch.history(limit=200)]
+            messages = [msg async for msg in ch.history(limit=100)]
             pointer = None
             for msg in messages:
                 if message.reference.resolved.content in msg.content:
                     pointer = msg
                     break
             if pointer: 
-                await ch.send(payload + f" **Replied:** {message.content}".replace("@everyone", "@ evryone"), files=att, reference=pointer)
+                await ch.send(payload + f" **Replied:** {message.content}", files=att, reference=pointer)
             else:
-                await ch.send(payload + f" **Replied:** {message.content}".replace("@everyone", "@ evryone"), files=att)
-        try:
-            await self.get_guild(log_guild).delete_emoji(emoji)
-        except:
-            pass
+                await ch.send(payload + f" **Replied:** {message.content}", files=att)
+
 
     async def on_message_edit(self, before, after):
-        if after.author.id == self.user.id:
+        if after.author.id == self.user.id or after.guild.id == log_guild:
             return
-        try:
-            ch = await self.create_on_events(after.channel)
-        except:
-            return
+        ch = await self.create_get_channel(after.channel)
         
-        payload = f"`UPD` **Updated:** {after.content}".replace("@everyone", "@ evryone")
-        messages = [msg async for msg in ch.history(limit=200)]
+        after.content = self.parse_content(after.content)
+        payload = f"`UPD` **Updated:** {after.content}"
+        messages = [msg async for msg in ch.history(limit=100)]
         pointer = None
         for msg in messages:
             if before.content in msg.content:
@@ -118,15 +101,13 @@ class Multi_Server_Logger(selfcord.Client):
             await ch.send(payload)
 
     async def on_message_delete(self, message):
-        if message.author.id == self.user.id:
+        if message.author.id == self.user.id or message.guild.id == log_guild:
             return
-        try:
-            ch = await self.create_on_events(message.channel)
-        except:
-            return
+
+        ch = await self.create_get_channel(message.channel)
         
         payload = "`DEL` **Deleted**"
-        messages = [msg async for msg in ch.history(limit=200)]
+        messages = [msg async for msg in ch.history(limit=100)]
         pointer = None
         for msg in messages:
             if message.content in msg.content:
@@ -136,14 +117,6 @@ class Multi_Server_Logger(selfcord.Client):
             await ch.send(payload, reference=pointer)
         else:
             await ch.send(payload)
-    
-    async def on_member_ban(self, guild, user):
-        try:
-            ch = await self.create_on_events(guild.channel)
-        except:
-            ch = self.get_channel(aux_channel)
-        payload = f"`BAN` **Banned:** {user.name}#{user.discriminator}"
-        await ch.send(payload)
 
         
 client = Multi_Server_Logger()
