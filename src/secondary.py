117c3685763074a94b4ac9c3b06bf9b567f22860
NYARENARD <82282414+NYARENARD@users.noreply.github.com>
2022-11-17 20:35:02 +0300

    Update secondary.py

--- src/secondary.py
+++ src/secondary.py
@@ -2,6 +2,5 @@ import os
 
 config = {
     "token_selfbot" : os.getenv("TOKEN_SELFBOT"),
-    "log_guild" : int(os.getenv("LOG_GUILD")),
-    "aux_channel" : int(os.getenv("AUX_CHANNEL"))
+    "log_guild" : int(os.getenv("LOG_GUILD"))
 }
